f99314f129d9b6d4518c22b531b9b504ba230bdf
rickybecerra <a01564070@tec.mx>
2026-09-17 14:21:57 -0600

    added basepath to hono

--- supabase/functions/api/app.js
+++ supabase/functions/api/app.js
@@ -8,7 +8,7 @@ import { moderationRoutes } from './routes/moderation.js';
 import { retentionRoutes } from './routes/retention.js';
 import { identityRoutes } from './routes/identity-routes.js';
 
-const app = new Hono().basePath('/api');
+const app = new Hono().basePath('/api');//all endpoints in Hono will have the prefix /api
 
 app.use('*', requestId);
 app.use('*', optionalAuth);
